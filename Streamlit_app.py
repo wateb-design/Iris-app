43106a3acd6b5453b0fa3bd5954ed77528b69cfe
wateb-design <watebvision@gmail.com>
2025-02-14 12:36:38 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import pandas as pd
 import altair as alt
+import plotly.express as px
 
-st.title("WELCOME TO WAFFO DASHBORD FOR IRIS WORK 2025")
+#st.title("WELCOME TO WAFFO DASHBORD FOR IRIS WORK 2025")
 #st.subheader("Creating dashboard...")
 #st.write("hello world")
 
