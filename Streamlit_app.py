b76b55b30f9640522ba246db40615391c48f67a5
wateb-design <watebvision@gmail.com>
2025-02-14 13:47:06 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,13 +1,13 @@
 pip install seaborn
 import streamlit as st
 import pandas as pd
-import seaborn as sns
+#import seaborn as sns
 import altair as alt
 
 #st.title("WELCOME TO WAFFO DASHBORD FOR IRIS WORK 2025")
 #st.subheader("Creating dashboard...")
 #st.write("hello world")
 
-df = sns.load_dataset('Iris')
-st.subheader('Aperçu des données')
-st.dataframe(df.head())
+#df = sns.load_dataset('Iris')
+st.set_page_config(page_title="Dashboard avec Menu Latéral", layout="wide")
+
