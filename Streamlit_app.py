633347fc39f0dc86978b030b883fdb3310b4b9b6
wateb-design <watebvision@gmail.com>
2025-02-14 12:04:01 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
-st.title("WELCOME TO WAFFO DASHBORD FOR IRIS WORK 2025")
-st.subheader("Creating dashboard...")
-st.write("hello world")
+#st.title("WELCOME TO WAFFO DASHBORD FOR IRIS WORK 2025")
+#st.subheader("Creating dashboard...")
+#st.write("hello world")
+
+st.set_page_config(
+    page_title="US WAFFO DASHBORD FOR IRIS APP",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
