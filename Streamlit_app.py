f95c044354c05cd186e774d5b680eb010a68ac4c
wateb-design <watebvision@gmail.com>
2025-02-15 10:29:42 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.set_page_config(page_title="Prédiction de Fleur Iris", layout="wide")
+
+st.sidebar.title("Menu de Navigation")
+menu = st.sidebar.radio("Choisir une option", ["Accueil", "Prédiction de Fleur", "A propos"])
